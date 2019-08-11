f85544f4e4117f4f5473cb5c82f7571be6ce3341
ShadoofJumper <ShadoofJumper@gmail.com>
2019-08-11 19:45:53 +0300

    Create blended sky box
    Create blended skybox, simple controller for programm changing skybox blend, aslo start make agaer for connect internet and cnhage sky box

---
+++ Assets/Scripts/WheatherController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+
+    private float _fullIntensity;
+    private float _cloudeValue = 0f;
+
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+
+    }
+
+
+    void Update()
+    {
+        SetOvercast(_cloudeValue);
+        _cloudeValue += .005f;
+    }
+
+    private void SetOvercast(float value)
+    {
+        if (value <= 1)
+        {
+            sky.SetFloat("_Blend", value);
+            sun.intensity = _fullIntensity - (_fullIntensity * value);
+        }
+
+    }
+}
