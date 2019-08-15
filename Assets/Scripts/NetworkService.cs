b125c1e283b18dace387d972fefc5badfc13aa36
ShadoofJumper <ShadoofJumper@gmail.com>
2019-08-15 21:19:11 +0300

    Learning how to get info  from internet
    Fix code, so i can get info using wheather api, elso create a messager so i can send info that i already have data to all object on scene.

--- Assets/Scripts/NetworkService.cs
+++ Assets/Scripts/NetworkService.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class NetworkService
 {
-    private const string xmlApi = 
-        "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&mode=xml";
+    // weather api http://openweathermap.org/api
+    private const string xmlApi = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0a8994dca3365d66ad0b10ee0baafc7e";
+        //"http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&mode=xml";
 
     private bool IsResponseValid(WWW www)
     {
-        if (www.error !=null)
+        Debug.Log(www.text);
+        Debug.Log(www.error);
+        if (!string.IsNullOrEmpty(www.error))
         {
             Debug.Log("Bad connect");
             return false;
--- Assets/Scripts/WeatherManager.cs
+++ Assets/Scripts/WeatherManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Xml;
 
 public class WeatherManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
+    public float cloudValue { get; private set; }
     // 
     private NetworkService _network;
 
@@ -19,8 +22,17 @@ public class WeatherManager : MonoBehaviour, IGameManager
 
     public void OnXMLDataLoaded(string data)
     {
-        Debug.Log(data);
+        XmlDocument doc = new XmlDocument(); // create class example
+        doc.LoadXml(data); // pars a xml code
+        XmlNode root = doc.DocumentElement;
 
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100f;
+
+        Debug.Log("Value: "+cloudValue);
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
         status = ManagerStatus.Started;
     }
 }
